52ef6f0558e31f9521c1944022c7d3027d5db553
Rebellant <158089230+Rebellant@users.noreply.github.com>
2025-06-09 21:01:39 +0200

    PMENU

--- krakovdragon/GameFolder/Scripts/Core/GameManager.gd
+++ krakovdragon/GameFolder/Scripts/Core/GameManager.gd
@@ -1,24 +1,35 @@
 extends Node
-
+#UI SIGNALS
 signal gained_coins(int)
 signal health_changed()
 
-var current_checkpoint: Checkpoint
+#OTHER VARS
 var player : Player
+var paused = false
+var pause_menu
+
+# TRIGGER VARS
 var is_dialogue_active = false
 var level_portal_active = false
 var knows_npc_lvl1 = false
 var knows_npc_lvl2 = false
 var player_health = 0
 var player_max_health = 180
-
 var dissapear_npc2 = false
 
-
+#GAME PROGRESS VARS
 var final_boss_dead = false
 var coins : int
 var checkpoint_counter=0
 var player_in_lvl=1
+var current_checkpoint: Checkpoint
+
+#FUNCS
+func _ready():
+	set_process_mode(Node.PROCESS_MODE_WHEN_PAUSED)
+
+func _process(delta):
+	print("GameManager activo durante pausa")
 
 func respawn_player():
 	if current_checkpoint != null:
@@ -37,3 +48,18 @@ func take_coins(coins_gained:int):
 	coins -= coins_gained
 	emit_signal("gained_coins", coins_gained)
 	print("Coins gained: ",coins)
+
+#PAUSE MENU LOGIC
+func pause_play():
+		paused = !paused
+		pause_menu.visible = paused
+
+func resume():
+	pause_play()
+	pause_menu.visible = false
+
+func save():
+	pass
+
+func main_menu():
+	get_tree().change_scene_to_file("res://GameFolder/Scenes/menu.tscn")
--- krakovdragon/GameFolder/Scripts/UI/ui_manager.gd
+++ krakovdragon/GameFolder/Scripts/UI/ui_manager.gd
@@ -1,15 +1,33 @@
 extends CanvasLayer
 
 
+
 func _ready():
 	$HealthBar.min_value = 0
 	$HealthBar.max_value = GameManager.player_max_health
 	GameManager.gained_coins.connect(update_coin_display)
+	GameManager.pause_menu = $PauseMenu
+
 
 	
-func _process(delta):
+func _process(_delta):
 	$HealthBar.value = GameManager.player_health
 	$CoinDisplay.text = str(GameManager.coins)
+	if Input.is_action_just_pressed("pause"):
+		GameManager.pause_play()
+		
 	
 func update_coin_display(gained_coins):
 	$CoinDisplay.text = str(GameManager.coins)
+
+
+func _on_btn_resume_pressed() -> void:
+	GameManager.pause_play()
+
+
+func _on_btn_save_pressed() -> void:
+	pass # Replace with function body.
+
+
+func _on_btn_main_menu_pressed() -> void:
+	get_tree().change_scene_to_file("res://GameFolder/Scenes/menu.tscn")
